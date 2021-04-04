52bcf8e77db81e7d6761c3233aed29e5ff64cacc
Jon Yeatman <jonlyeatman@gmail.com>
2021-04-04 17:27:23 -0400

    JS modal prep

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// git modal elements
+let modal = document.getElementById('simpleModal');
+
+// get open modal button 
+let modalBtn = document.getElementById('modalBtn');
+
+// get close button 
+let closeBtn = document.getElementById('closeBtn');
+
+// listen for click
