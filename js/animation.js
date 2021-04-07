2dcdb6c885573e5bfdbe3c292fd9febe13c559ff
Jon Yeatman <jonlyeatman@gmail.com>
2021-04-07 19:08:55 -0400

    fixed oversized on mobile issue

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,7 @@
-gsap.fromTo(".title-text", {opacity: 0.1, y:-25}, 
-{opacity: 1, y:0, scale:1.2, duration: 2.5});
+gsap.fromTo(".title-text", {opacity: 0, y:-25, scale:0}, 
+    {opacity: 1, y:0, scale:1, duration: 2.5});
 
 
 
-gsap.fromTo(".button-group", {opacity: 0, y:80}, {opacity: 1, y:0, duration: 3});
+gsap.fromTo(".button-group", {opacity: 0, y:100, scale:0}, 
+    {opacity: 1, y:0, scale:1, duration: 2});
