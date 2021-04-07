f80f5643ca16edb508f68f1ba65c2d9f531e682a
Jon Yeatman <jonlyeatman@gmail.com>
2021-04-07 18:45:45 -0400

    Green sock animation initial transitions

---
+++ js/animation.js
@@ -0,0 +1,6 @@
+gsap.fromTo(".title-text", {opacity: 0.1, y:-25}, 
+{opacity: 1, y:0, scale:1.2, duration: 2.5});
+
+
+
+gsap.fromTo(".button-group", {opacity: 0, y:80}, {opacity: 1, y:0, duration: 3});
