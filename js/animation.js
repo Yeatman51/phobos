fd0d74e0306ee3ce73349a7dc49a586de5313bae
jon <jonlyeatman@gmail.com>
2021-12-08 15:51:11 -0500

    Element styling sizing and animations

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,8 @@
 gsap.fromTo(".title-text", {opacity: 0, y:-25, scale:0}, 
     {opacity: 1, y:0, scale:1, duration: 2.5});
 
-
+gsap.fromTo(".weather-container", {opacity: 0, y:0, scale:0}, 
+   {opacity: 1, y:0, scale:1, duration: 2});
 
 gsap.fromTo(".button-group", {opacity: 0, y:100, scale:0}, 
-    {opacity: 1, y:0, scale:1, duration: 2});
+    {opacity: 1, y:0, scale:1, duration: 2.8});
