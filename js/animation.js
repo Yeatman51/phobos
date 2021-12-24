c8ee14bcd3d0c4e78a11e79fa8d314a144edbfd5
jon <jonlyeatman@gmail.com>
2021-12-23 23:13:12 -0500

    mars weather setup

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,11 @@
 gsap.fromTo(".title-text", {opacity: 0, y:-25, scale:0}, 
-    {opacity: 1, y:0, scale:1, duration: 2.5});
+   {opacity: 1, y:0, scale:1, duration: 2.5});
+
+gsap.fromTo(".weather-title", {opacity: 0, y:0, scale:0}, 
+   {opacity: 1, y:0, scale:1, duration: 2});
 
 gsap.fromTo(".weather-container", {opacity: 0, y:0, scale:0}, 
    {opacity: 1, y:0, scale:1, duration: 2});
 
 gsap.fromTo(".button-group", {opacity: 0, y:100, scale:0}, 
-    {opacity: 1, y:0, scale:1, duration: 2.8});
+   {opacity: 1, y:0, scale:1, duration: 2.8});
